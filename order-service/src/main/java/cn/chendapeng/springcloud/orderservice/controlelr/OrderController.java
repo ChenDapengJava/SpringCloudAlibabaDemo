6fd019a59edb0b2f04353d663cbb88087b03b2e6
行百里er <cpeng8912@gmail.com>
2022-08-22 22:29:24 +0800

    增加 order-service 模块

---
+++ order-service/src/main/java/cn/chendapeng/springcloud/orderservice/controlelr/OrderController.java
@@ -0,0 +1,27 @@
+package cn.chendapeng.springcloud.orderservice.controlelr;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 博客：https://chendapeng.cn - 行百里者半九十，做事要善始善终！
+ * 公众号：行百里er
+ *
+ * @author 行百里者
+ * @date 2022-08-22 21:37
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/info/{id}")
+    public String getOrderInfo(@PathVariable("id") Long id) {
+        return "order" + id + "，Server Port：" + serverPort;
+    }
+}
