bad17f4c164da8ddd329173314ba6f4b7a0bb23b
行百里er <cpeng8912@gmail.com>
2022-08-26 21:01:28 +0800

    公共使用库

--- internal-common/src/main/java/cn/chendapeng/springcloud/common/InternalCommonApplication.java
+++ internal-common/src/main/java/cn/chendapeng/springcloud/common/InternalCommonApplication.java
@@ -3,6 +3,9 @@ package cn.chendapeng.springcloud.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chendapeng.cn
+ */
 @SpringBootApplication
 public class InternalCommonApplication {
 
