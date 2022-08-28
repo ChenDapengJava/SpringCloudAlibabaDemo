85536988a4cab84a91ae6cf4396df76eade409bc
行百里er <cpeng8912@gmail.com>
2022-08-28 19:31:55 +0800

    客户端注册到admin server

--- spring-boot-admin-server/src/main/java/cn/chendapeng/springcloud/springbootadminserver/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/cn/chendapeng/springcloud/springbootadminserver/SpringBootAdminServerApplication.java
@@ -1,9 +1,11 @@
 package cn.chendapeng.springcloud.springbootadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminServerApplication {
 
     public static void main(String[] args) {
---
+++ spring-boot-admin-server/src/main/java/cn/chendapeng/springcloud/springbootadminserver/config/ChenDapengHealthIndicator.java
@@ -0,0 +1,29 @@
+package cn.chendapeng.springcloud.springbootadminserver.config;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 博客：https://chendapeng.cn - 行百里者半九十，凡是善始善终，吾将上下而求索！
+ * 公众号：行百里er
+ *
+ * @author 行百里者
+ * @date 2022-08-28 17:13
+ */
+@Component
+public class ChenDapengHealthIndicator extends AbstractHealthIndicator {
+
+    /**
+     * 自定义健康检查输出信息
+     */
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.up()
+                .withDetail("status", "Oj8K!")
+                .withDetail("whoami", "外星人")
+                .withDetail("uptime", new Date());
+    }
+}
