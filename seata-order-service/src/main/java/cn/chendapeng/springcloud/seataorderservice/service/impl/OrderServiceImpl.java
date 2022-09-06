434885e4f9e080944287a9429774f6170408a8bb
行百里er <cpeng8912@gmail.com>
2022-09-06 22:24:08 +0800

    Seata AT模式

--- seata-order-service/src/main/java/cn/chendapeng/springcloud/seataorderservice/service/impl/OrderServiceImpl.java
+++ seata-order-service/src/main/java/cn/chendapeng/springcloud/seataorderservice/service/impl/OrderServiceImpl.java
@@ -38,23 +38,25 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
      * 分布式环境下，可以只保留 @GlobalTransactional 注解
      */
     @Override
-    @GlobalTransactional
+    //@GlobalTransactional
     @Transactional(rollbackFor = Exception.class)
     public void createOrder(Order order) {
+        log.info("开始扣减库存，skuId={}", order.getSkuId());
+        // 扣减库存
+        wareFeignClient.deductStock(order.getSkuId());
+        log.info("扣减库存完成，skuId={}", order.getSkuId());
+
         // 订单号
         String orderSn = IdWorker.getTimeId();
         order.setOrderSn(orderSn);
         order.setCreateTime(new Date());
 
         log.info("开始创建订单:{}", order);
+        // 此处异常使用 Spring 的 @Transactional 就可以解决
+        log.error("此处添加异常order.getId()此时为null，模拟分布式事务出现：{}", order.getId().toString());
         // 创建订单
         orderMapper.insert(order);
 
-        // 此处异常使用 Spring 的 @Transactional 就可以解决
-        //log.error("此处添加异常，模拟分布式事务出现：{}", order.getId() / 0);
-
-        // 扣减库存
-        wareFeignClient.deductStock(order.getSkuId());
         log.info("创建订单完成");
     }
 }
